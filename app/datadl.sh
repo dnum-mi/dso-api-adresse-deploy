008b1894e45505d5eb57c90826377df7abbf9c75
Baptiste Noleau <bnoleau@adlere.fr>
2024-08-02 10:37:03 +0200

    wip

--- app/datadl.sh
+++ app/datadl.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 ## Download data from S3 to specific volume
 
+echo "Download data:"
+echo "  from: ${S3_ENDPOINT_URL} ${S3_BUCKET}/${S3_FILE}"
+echo "  to: ${VOLDATA}/${S3_FILE}"
+
 mkdir -p ${VOLDATA}
+
+echo 'STEP 1/1: downlaod data'
 
 aws ${AWS_OPTS} --endpoint-url ${S3_ENDPOINT_URL} s3 cp ${S3_BUCKET}/${S3_FILE} ${VOLDATA}/${S3_FILE}
 
+if [ $? -ne 0 ]; then
+  echo "Impossible de télécharger les fichiers depuis le S3"
+  exit 1
+fi
--- app/dataloader.sh
+++ app/dataloader.sh
@@ -3,12 +3,36 @@
 
 ADDOK_DIRECTORY=addok
 
+echo "Download data:"
+echo "  from ${ADDOK_FRANCE_BUNDLE_URL}"
+echo "  to ${S3_ENDPOINT_URL} ${S3_BUCKET}"
+
 cd /data/
+
+echo 'STEP 1/3: download data'
 wget ${ADDOK_FRANCE_BUNDLE_URL} -O addok-france-bundle.zip
+
+if [ $? -ne 0 ]; then
+  echo 'Impossible de récupérer le fichier sur internet'
+  exit 1
+fi
 
 mkdir ${ADDOK_DIRECTORY}
 
+echo 'STEP 2/3: unzip data'
 unzip addok-france-bundle.zip -d ${ADDOK_DIRECTORY}
 
+if [ $? -ne 0 ]; then
+  echo "Impossible de dézipper l'archive"
+  exit 1
+fi
+
+ls -lh ${ADDOK_DIRECTORY}
+
+echo 'STEP 3/3: upload data to S3'
 aws ${AWS_OPTS} --endpoint-url ${S3_ENDPOINT_URL} s3 cp --recursive ${ADDOK_DIRECTORY}/ ${S3_BUCKET}
 
+if [ $? -ne 0 ]; then
+  echo "Impossible d'uploader les fichiers sur le S3"
+  exit 1
+fi
